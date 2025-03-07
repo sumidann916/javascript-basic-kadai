8faedeaaeddb2467bfcaaadafc803133ead6b198
sumidann916 <165361062+sumidann916@users.noreply.github.com>
2025-03-07 13:46:01 +0900

    Delete kadai_020 directory

--- kadai_020/event.js
+++
@@ -1,7 +0,0 @@
-const button = document.getElementById(`btn`);
-const change_text = document.getElementById(`text`);
-
-btn.addEventListener(`click`, () => {
-  console.log(`クリックされました！`);
-  change_text.textContent = `ボタンがクリックされました！`;
-});
